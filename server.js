09471d1267f421d77f34f3c227a0898ea9b6d15e
d4160 <d4160@outlook.com>
2021-08-06 09:21:59 -0500

    Public ip 2

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const https = require('https');
 const path = require('path');
 const fs = require('fs');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(helmet()); // Add Helmet as a middleware
 
 // Serve static files
 app.use(express.static(__dirname + '/dist/ucvr-web'));
@@ -17,6 +20,6 @@ https.createServer({
   key: fs.readFileSync('/etc/ssl/conti/llave_continental2021.key'),
   cert: fs.readFileSync('/etc/ssl/conti/continental.crt')
 }, app)
-.listen(6000, '0.0.0.0', function () {
+.listen(6000, '172.16.3.252', function () {
   console.log('VRUC app listening on port 6000.')
 });
