40f6ba25976fd17f2072152b90849c189cd7482d
d4160 <d4160@outlook.com>
2021-08-06 09:26:42 -0500

    Change to port 443

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ https.createServer({
   key: fs.readFileSync('/etc/ssl/conti/llave_continental2021.key'),
   cert: fs.readFileSync('/etc/ssl/conti/continental.crt')
 }, app)
-.listen(6000, '172.16.3.252', function () {
-  console.log('VRUC app listening on port 6000.')
+.listen(443, '172.16.3.252', function () {
+  console.log('VRUC app listening on port 443.')
 });
